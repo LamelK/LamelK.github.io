8cd25c4ad0accfd0c07da069ace492061d4b24e9
Lamel <lamel466@gmail.com>
2025-06-08 17:54:45 +0200

    d

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // For your specific case (LamelK.github.io)
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
